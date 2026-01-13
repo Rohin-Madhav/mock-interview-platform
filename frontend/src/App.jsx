abcd10257e61200e65a3cec433c4a2a6192d32ed
Rohin Madhav <rohinmadhavk7@gmail.com>
2026-01-13 13:12:51 +0530

    added pages and routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
+import { BrowserRouter,Routes,Route } from 'react-router-dom'
+import Layout from './components/Layout'
+import Home from './pages/Home'
+import About from './pages/About'
+import Blog from './pages/Blog'
+import Feedback from './pages/Feedback'
+import Contact from './pages/Contact'
 
 function App() {
   return (
-    <div className='text-red-600'>
-      <h1>Hello world</h1>
-    </div>
+    < >
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Layout/>}>
+      <Route index element={<Home/>}/>
+      <Route path='about' element={<About/>}/>
+      <Route path='blog' element={<Blog/>}/>
+      <Route path='feedback' element={<Feedback/>}/>
+      <Route path='contact-us' element={<Contact/>}/>
+      </Route>
+
+    </Routes>
+    </BrowserRouter>
+    </>
   )
 }
 
